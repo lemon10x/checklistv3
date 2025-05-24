52088432bbcfd689bc1d4bea9b3cdc8a8f25a0b6
lemon10x <88604444+lemon10x@users.noreply.github.com>
2025-05-24 00:25:45 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:excel/excel.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+// Código completo que teníamos antes (se colocará aquí)
